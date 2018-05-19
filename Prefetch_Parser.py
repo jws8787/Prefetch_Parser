bd9aed8de19090bd83ff49bfa3b104550ed3b369
jws <jwsxzx@naver.com>
2018-05-20 01:14:05 +0900

    finish

--- Prefetch_Parser.py
+++ Prefetch_Parser.py
@@ -17,7 +17,7 @@ PF_file_Name, PF_file_Size, PF_file_Last_Run_Time, PF_file_Created_Time, PF_file
 
 cut = 0
 
-f = open('Prefetch_info.csv', 'w', encoding='utf-8')
+f = open('Prefetch_info.csv', 'w', encoding='utf-8', newline='')
 wr = csv.writer(f)
 wr.writerow(["PF_File_Name", "File_Name", "File_Size", "Last_Run_Time", "Created_Time", "Modified_Time", "Run_Count"])
 
@@ -26,13 +26,13 @@ for PF_file in PF_file_list:
 	try:
 		PF_file_Name.append("".join(map(chr,Data[16:75])))
 		PF_file_Size.append(struct.unpack_from("<i",(Data[0x0C:]))[0])
-		PF_file_Last_Run_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x80:])[0]).strftime('%Y-%m-%d %H:%M:%S.%f'))
-		PF_file_Created_Time.append(datetime.fromtimestamp(os.path.getctime(PF_file)).strftime('%Y-%m-%d %H:%M:%S.%f'))
-		PF_file_Modified_Time.append(datetime.fromtimestamp(os.path.getmtime(PF_file)).strftime('%Y-%m-%d %H:%M:%S.%f'))
+		PF_file_Last_Run_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x80:])[0]).strftime('%Y:%m:%d_%H:%M:%S.%f'))
+		PF_file_Created_Time.append(datetime.fromtimestamp(os.path.getctime(PF_file)).strftime('%Y:%m:%d_%H:%M:%S.%f'))
+		PF_file_Modified_Time.append(datetime.fromtimestamp(os.path.getmtime(PF_file)).strftime('%Y:%m:%d_%H:%M:%S.%f'))
 		PF_file_Run_Count.append(struct.unpack_from("<i", Data[0xD0:])[0])
 		
 		print('[*]File_Name:'+ str(PF_file_Name[cut]))
-		print('[**]File_Size:' + str(PF_file_Size[cut]))
+		print('[**]File_Size:' + str(PF_file_Size[cut])+'byte')
 		print('[***]Last_Run_Time:' + str(PF_file_Last_Run_Time[cut]))
 		print('[****]Created_Time:' + str(PF_file_Created_Time[cut]))
 		print('[*****]Modified_Time:' + str(PF_file_Modified_Time[cut]))
@@ -48,6 +48,9 @@ cut = 0
 
 for PF in PF_file_Name_List:
 	if PF.endswith('.pf'):
-		wr.writerow([PF, PF_file_Name[cut], PF_file_Size[cut], PF_file_Last_Run_Time[cut], PF_file_Created_Time[cut], PF_file_Modified_Time[cut], PF_file_Run_Count[cut]])
+		wr.writerow([PF, PF_file_Name[cut], str(PF_file_Size[cut])+' byte', PF_file_Last_Run_Time[cut], PF_file_Created_Time[cut], PF_file_Modified_Time[cut], PF_file_Run_Count[cut]])
 		cut+=1
+
+f.close()
+
 print('Finished')
