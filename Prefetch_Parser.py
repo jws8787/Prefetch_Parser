2d8e9c8860d634f2b380aa18a3b04f5bce2711cc
jws8787 <jws8787@naver.com>
2018-05-20 00:56:56 +0900

    result

--- Prefetch_Parser.py
+++ Prefetch_Parser.py
@@ -1,25 +1,53 @@
 import os
-import sys
+import csv
 import glob
-import datetime
+from datetime import datetime, timedelta
+import struct
 from XPRESS_decompress import *
 
-def LittleEndian_Int(buf):
-	Val = 0
-	for i in range(0, len(buf)):
-		Multi = 1
-		for j in range(0, i):
-			Multi *= 256
-		Val += buf[i] * Multi
-	return Val
+def Win_ts(timestamp):
+    WIN32_EPOCH = datetime(1601, 1, 1)
+    return WIN32_EPOCH + timedelta(microseconds=timestamp//10, hours=9)
 
 Dir_path=sys.argv[1]
 PF_file_list =  glob.glob(Dir_path+'/*.pf')
+PF_file_Name_List = os.listdir(Dir_path)
 
-PF_file_Name, PF_file_Size, PF_file_Last_Run_Time = [],[],[]
+PF_file_Name, PF_file_Size, PF_file_Last_Run_Time, PF_file_Created_Time, PF_file_Modified_Time, PF_file_Run_Count = [],[],[],[],[],[]
+
+cut = 0
+
+f = open('Prefetch_info.csv', 'w', encoding='utf-8')
+wr = csv.writer(f)
+wr.writerow(["PF_File_Name", "File_Name", "File_Size", "Last_Run_Time", "Created_Time", "Modified_Time", "Run_Count"])
 
 for PF_file in PF_file_list:
 	Data = decompress(PF_file)
-	PF_file_Name.append(Data[16:75])
-	PF_file_Size.append(LittleEndian_Int(Data[12:15]))
-	
+	try:
+		PF_file_Name.append("".join(map(chr,Data[16:75])))
+		PF_file_Size.append(struct.unpack_from("<i",(Data[0x0C:]))[0])
+		PF_file_Last_Run_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x80:])[0]).strftime('%Y-%m-%d %H:%M:%S.%f'))
+		PF_file_Created_Time.append(datetime.fromtimestamp(os.path.getctime(PF_file)).strftime('%Y-%m-%d %H:%M:%S.%f'))
+		PF_file_Modified_Time.append(datetime.fromtimestamp(os.path.getmtime(PF_file)).strftime('%Y-%m-%d %H:%M:%S.%f'))
+		PF_file_Run_Count.append(struct.unpack_from("<i", Data[0xD0:])[0])
+		
+		print('[*]File_Name:'+ str(PF_file_Name[cut]))
+		print('[**]File_Size:' + str(PF_file_Size[cut]))
+		print('[***]Last_Run_Time:' + str(PF_file_Last_Run_Time[cut]))
+		print('[****]Created_Time:' + str(PF_file_Created_Time[cut]))
+		print('[*****]Modified_Time:' + str(PF_file_Modified_Time[cut]))
+		print('[******]Run_Count: '+ str(PF_file_Run_Count[cut]))
+		print('\n')
+		
+		cut +=1
+
+	except WindowsError:
+		pass
+
+cut = 0
+
+for PF in PF_file_Name_List:
+	if PF.endswith('.pf'):
+		wr.writerow([PF, PF_file_Name[cut], PF_file_Size[cut], PF_file_Last_Run_Time[cut], PF_file_Created_Time[cut], PF_file_Modified_Time[cut], PF_file_Run_Count[cut]])
+		cut+=1
+print('Finished')
